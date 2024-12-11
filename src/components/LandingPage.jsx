a946c358cae7652d47ce488f4764d518ae0090d9
Salal Saleem <salalsaleem2420@gmail.com>
2024-12-11 20:39:41 +0500

    updated

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function LandingPage() {
   return (
-    <div className='w-full h-screen'>LandingPage</div>
+    <div className='w-full h-screen bg-zinc-900 pt-2'>
+      <div className='textstructure mt-40 px-20'>
+        <div className='masker'>
+          <h1 className="uppercase leading-none tracking-tight text-9xl font-['Founders_Grotesk'] font-semibold">We Create</h1>
+        </div>
+      </div>
+    </div>
   )
 }
 
