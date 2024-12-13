9eaf8e94ab66460d04d930677003b22695668773
Salal Saleem <salalsaleem2420@gmail.com>
2024-12-13 22:01:51 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/navbar'
 import LandingPage from './components/LandingPage'
+import Marquee from './components/Marquee'
 
 function App() {
   return (
     <div className="w-full h-screen text-white">
       <Navbar/>
       <LandingPage/>
+      <Marquee/>
     </div>
   )
 }
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaArrowRight } from "react-icons/fa6";
 
 function LandingPage() {
   return (
@@ -6,14 +7,18 @@ function LandingPage() {
       <div className='textstructure mt-40 px-20'>
         {["We Create","Eye-opening","presentations"].map((item,key)=>{
           return <div className='masker'>
+            <div className='w-fit flex'>
+            {key === 1 && (<div className='w-[9vw] rounded mx-2 h-[5vw] bg-red-700 relative'></div>)}
           <h1 key={key} className="uppercase leading-[4.7vw] tracking-tight text-7xl font-['Founders_Grotesk'] font-semibold">{item}</h1>
+          </div>
         </div>
         })}
        </div>
        <div className='border-t-[1px] mt-36 flex justify-between py-4 px-10 text-sm'>
         {["For public and private companies","From the first pitch to IPO"].map((items,key)=><p key={key}>{items}</p>)}
-       <div>
-        <div className='border-[1px] rounded px-2 py-1'><p>Start the project</p></div>
+       <div className='flex'>
+        <div className='border-[1px] rounded-full px-3 py-1'><p>Start the project</p></div>
+        <div className='flex justify-center items-center border-[1px] rounded-full ml-2 px-2 py-1'><span className='-rotate-45'><FaArrowRight /></span></div>
        </div>
        </div>
     </div>
