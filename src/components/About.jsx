3499476f0c83ae94b142dcd6720f5f300aefe53d
Salal Saleem <salalsaleem2420@gmail.com>
2024-12-20 16:40:21 +0500

    updated

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='bg-[#CDEA68] p-20'>
+        <h1 className='text-black text-[3.2vw] leading-[3.3vw]'>Ochi is a strategic presentation agency for forward-thinking businesses that need to raise funds, sell prod­ucts, ex­plain com­plex ideas, and hire great peo­ple.</h1>
+    </div>
+  )
+}
+
+export default About
--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Marquee() {
   return (
 
-    <div className='w-full h-[50vh] bg-green-800 pt-20'>
+    <div className='w-full h-[50vh] bg-[#004D43] pt-20'>
         <div className='text flex text-nowrap overflow-hidden border-t-[1px] border-b-[1px] py-5 font-semibold border-zinc-300'>
             <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease:"linear", repeat:Infinity , duration : 5}} className='text-9xl	leading-none uppercase'>We are ochi</motion.h1>
             <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease:"linear", repeat:Infinity , duration : 5}} className='text-9xl	leading-none uppercase'>We are ochi</motion.h1>
