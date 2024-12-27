c13e1c8c1104c861eb04abaf32626a49b02b9767
Salal Saleem <salalsaleem2420@gmail.com>
2024-12-28 00:02:43 +0500

    updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,9 +13,8 @@ function About() {
           <img src="https://ochi.design/wp-content/uploads/2022/05/Homepage-Photo-663x469.jpg" className='rounded-lg' width={"100%"} />
           </div>
         </div>
-        ////dsaiusdak/////
         <div className='w-full h-screen'>
-      <div style={{backgroundImage: `url("https://via.placeholder.com/500")`}}></div>
+      <div className='bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg")] w-full h-full bg-cover bg-center'></div>
         </div>
     </div>
   )
