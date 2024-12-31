028c7709c52f0fcbb5f3f477b89670e156ea20de
Salal Saleem <salalsaleem2420@gmail.com>
2024-12-31 23:37:44 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,21 @@ import LandingPage from './components/LandingPage'
 import Marquee from './components/Marquee'
 import About from './components/About'
 import Featured from './components/Featured'
+import LocomotiveScroll from 'locomotive-scroll';
+import ThreeCards from './components/ThreeCards'
 
 function App() {
+
+const locomotiveScroll = new LocomotiveScroll();
+
   return (
-    <div className="w-full h-screen text-white">
+    <div  className="w-full h-screen text-white">
       <Navbar/>
       <LandingPage/>
       <Marquee/>
       <About/>
       <Featured/>
+      <ThreeCards/>
     </div>
   )
 }
---
+++ src/components/ThreeCards.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function ThreeCards() {
+  return (
+    <div className='w-full flex flex-wrap p-20 h-screen bg-zinc-900'>
+        <div className='flex justify-center items-center w-1/2'>
+            <div className='bg-green-800 w-full h-[80%]'>
+                <img src="https://ochi.design/wp-content/uploads/2022/04/logo001.svg" alt="" />
+            </div>
+        </div>
+        <div className='bg-zinc-400 w-1/2 flex px-5'>
+            <div className='bg-orange-700 w-1/2 h-full mx-5 h-[80%]'> </div>
+            <div className='bg-purple-700 w-1/2 h-full h-[80%]'></div>
+        </div>
+    </div>
+  )
+}
+
+export default ThreeCards
