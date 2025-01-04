d12b008c2d0da00f7e90911e83cdd764f99bb370
Salal Saleem <salalsaleem2420@gmail.com>
2025-01-04 12:48:49 +0500

    updated

--- src/components/ThreeCards.jsx
+++ src/components/ThreeCards.jsx
@@ -4,40 +4,19 @@ function ThreeCards() {
   return (
     <div className='w-full flex flex-wrap p-20 h-screen bg-zinc-900'>
         <div className='flex justify-center items-center w-1/2'>
-            <div className='bg-green-800 w-full h-[80%]'>
+            <div className='bg-[#004D43] w-full flex flex-col p-5 justify-between h-[80%] rounded-lg'>
+              <div className='h-full flex items-center justify-center'>
                 <img src="https://ochi.design/wp-content/uploads/2022/04/logo001.svg" alt="" />
+                </div>
+                <div>
+                  <p className='text-[#CDEA68] border-2 w-fit py-[0.2vw] px-[0.5vw] rounded-full border-[#CDEA68]'>&#169;2019-2022</p>
+                </div>
             </div>
         </div>
-        <div className='bg-zinc-400 w-1/2 flex px-5'>
-            <div className='bg-orange-700 w-1/2 h-full mx-5 h-[80%]'> </div>
-            <div className='bg-purple-700 w-1/2 h-full h-[80%]'></div>
+        <div className='bg-zinc-400 w-1/2 flex items-center px-5'>
+            <div className='bg-orange-700 w-1/2 mx-5 h-[80%] rounded-lg'> </div>
+            <div className='bg-purple-700 w-1/2 h-[80%] rounded-lg'></div>
         </div>
-        ///// for push git only ////
-
-<div>
-
-  <h1>
-
-    <p>
-
-
-      <h1>
-
-
-        <kbd>
-
-
-          <h1>
-
-
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti id voluptatibus ab quaerat pariatur! Alias voluptates corporis libero reiciendis minima officia nemo, illum voluptatibus repudiandae doloribus nobis perferendis cum iste!</p>
-          </h1>
-        </kbd>
-      </h1>
-    </p>
-  </h1>
-</div>
-
     </div>
    
    
